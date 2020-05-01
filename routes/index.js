199f3bf7c025b355c86586bb7dd9c7f5d82118d0
Miguel Briones <migg.briones@gmail.com>
2020-05-01 17:49:51 -0500

    Se genera URLs del Proyecto, se agrega librería Slug para URL amigables, quita espacios y agrega guines medios al nombre del proyecto.

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,14 @@ const proyectosController = require('../controllers/proyectosController');
 
 
 module.exports = function() {
-    // ruta para el home
+    /* Se definen rutas, se puede observar que las peticiones pueden hacer hacia
+        la misma ruta, pero con diferente método Para obtener, agregar información.
+
+        Validación por medio de express-validator
+
+        Se puede tener para una misma ruta, más de dos controladores; sería
+        por orden de ejecución.
+    */
     router.get('/', proyectosController.proyectosHome);
     router.get('/nuevo-proyecto', proyectosController.formularioProyecto);
     router.post('/nuevo-proyecto', 
